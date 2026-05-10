b682123262497458eaf55376c3092704db5f0bce
artrittup <artrit.telaku@student.uni-pr.edu>
2026-05-10 15:55:47 +0200

    shtova indexin dhe readme
    shtova pjesen e index.php dhe bera lidhjen mes files tjere, po ashtu shtova nje readme te thjeshte

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/app/core.php';
+require __DIR__ . '/app/two_factor.php';
+require __DIR__ . '/app/auth.php';
+
+load_env(APP_ROOT . '/.env');
+init_storage();
+
+$page = $_GET['page'] ?? (is_logged_in() ? 'dashboard' : 'login');
+
+switch ($page) {
+    case 'register':
+        register_page();
+        break;
+    case 'verify_totp':
+        verify_totp_page();
+        break;
+    case 'verify_sms':
+        verify_sms_page();
+        break;
+    case 'verify_hardware':
+        verify_hardware_page();
+        break;
+    case 'dashboard':
+        dashboard_page();
+        break;
+    case 'logout':
+        logout_user();
+        break;
+    case 'login':
+    default:
+        login_page();
+        break;
+}
